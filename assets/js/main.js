544f3e96392db643826df32c1570dcc1ccbfe5f2
Nikita Koshik <nikitakoshik064@gmail.com>
2021-05-24 18:18:51 +0300

    25.05.2021 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,11 @@
 // $('.arr__down').click(function(){
 //     $(this).parent('.box').toggleClass('active');
 //   });
+let item = document.querySelector(".select__link");
+let select = document.querySelector(".sub-select__list");
+
+select.addEventListener("click", function(e) {
+    e = e.target;
+    if (!e.classList.contains("sub-select__link")) return;
+    item.innerHTML = e.innerHTML;
+});
